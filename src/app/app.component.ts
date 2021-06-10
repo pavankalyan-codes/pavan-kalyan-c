84f8e9d1d18cf63ea6efb50d777acf754d35a275
pavankalyan.c <pavankalyan141640@gmail.com>
2021-06-10 23:10:07 +0530

    Portfolio changes WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  colors: string[] = [];
+  typewriter_text: string = "Thank you for your interest";
+  typewriter_display: string = "";
+  skills: string[] = ["Front End Developer", "Quick Learner", "Problem Solver"];
+  ngOnInit() {
+    this.colors.push(this.getRandomGradient());
+    this.typingCallback();
+  }
+  title = "portfolio";
+
+  randomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  randomAngle() {
+    return Math.floor(Math.random() * 360) + "deg";
+  }
+
+  typingCallback() {
+    let idx = 0;
+    let up: boolean = true;
+    let n: number = 0;
+    setInterval(() => {
+      let word = this.skills[idx % this.skills.length];
+      let ln = word.length;
+
+      if (up) {
+        this.typewriter_display = word.slice(0, n);
+        n++;
+      }
+      if (n === ln + 1) {
+        up = false;
+      }
+      if (!up) {
+        this.typewriter_display = word.slice(0, n);
+        n = n - 2;
+        if (n < 0) n = 0;
+      }
+      if (n == 0) {
+        up = true;
+        idx++;
+      }
+      if (idx === this.skills.length) {
+        idx = 0;
+      }
+    }, 200);
+  }
+
+  getRandomGradient() {
+    return (
+      "linear-gradient(" +
+      this.randomAngle() +
+      "," +
+      this.randomColor() +
+      "," +
+      this.randomColor() +
+      ")"
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { MatIconModule } from "@angular/material/icon";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatIconModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
